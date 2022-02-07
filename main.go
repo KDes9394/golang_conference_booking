7510ec742f12dad3160d7258f350b77b5b602e90
lasj <kentousa@gmail.com>
2022-02-07 03:31:44 -0800

    2/5/21

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conference booking application")
-	fmt.Println("Get your tickets here to attend this")
 
 	var conferenceName = "Golang Conference"
-	fmt.Println(conferenceName)
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We currently have %v guests who booked tickets and %v spots remaining\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend this")
+
+	//inform when all 50 tickets are sold
+
 }
